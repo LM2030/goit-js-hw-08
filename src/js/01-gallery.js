52c6fe4bda303dfd5a607ed8648dee04b139063d
LM2030 <melnyk.ukr@gmail.com>
2022-11-18 16:29:30 +0200

    1-st Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryAll = document.querySelector('.gallery');
+const galleryCard = galleryItems.map(cardCreate).join('');
+
+galleryAll.insertAdjacentHTML('afterbegin', galleryCard);
+
+function cardCreate({ preview, original, description }) {
+  return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
